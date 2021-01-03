1910e804dcfafba9a42d99d1cef4a6ffae0e58ce
v61265 <v61265@gmail.com>
2021-01-03 15:13:30 +0800

    cors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 const port = 3001;
+var cors = require("cors");
+
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`hihihi`);
